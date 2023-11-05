275ea92be4ff8973f5084d3c084895cac2c0a28b
iamahens <sneha_223012@saitm.org>
2023-11-05 10:52:00 +0530

    Files uploaded

---
+++ main.js
@@ -0,0 +1,24 @@
+
+    var swiper = new Swiper(".bg-slider-thumbs", {
+      loop: true,
+      spaceBetween: 0,
+      slidesPerView: 0,
+    //   freeMode: true,
+    //   watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".bg-slider", {
+      loop: true,
+      spaceBetween: 10,
+    //   navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+
+    window.addEventListener("scroll", function(){
+      const header = document.querySelector("header");
+      header.classList.togglele("sticky", window.scrollY > 0);
+    })
